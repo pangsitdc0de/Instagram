1315278d6633de53591893cd90554040f58977de
pangsitdc0de <34424242+pangsitdc0de@users.noreply.github.com>
2017-12-11 10:30:03 +0700

    Update unfollall.js

--- unfollall.js
+++ unfollall.js
@@ -10,18 +10,18 @@ const User = [
 	{
 		type:'input',
 		name:'username',
-		message:'Insert Username'
+		message:'Username Lo'
 	},
 	{
 		type:'password',
 		name:'password',
-		message:'Insert Password',
-		mask:'*'
+		message:'Username Lo',
+		mask:'8'
 	},
 	{
 		type:'input',
 		name:'sleep',
-		message:'Insert Sleep (In MiliSeconds)',
+		message:'Waktu yang diinginkan (Reccomend min:380000MemekSecond)',
 		validate: function(value){
 			value = value.match(/[0-9]/);
 			if (value) return true;
@@ -49,35 +49,35 @@ const Login = async function(User){
 const Unfollow = async function(session, accountId){
 	try {
 		await Client.Relationship.destroy(session, accountId);
-		return chalk`{bold.green SUKSES}`;
+		return chalk`{bold.green TOPCER}`;
 	} catch (err){
-		return chalk`{bold.red GAGAL}`;
+		return chalk`{bold.red Sayang Sekali}`;
 	}
 }
 
 const Excute = async function(User,sleep){
 	
 	try {
-		console.log(chalk`{yellow [?] Try to Login ....}`);
+		console.log(chalk`{yellow [?] Sedang Login Mek ....}`);
 		const doLogin = await Login(User);
-		console.log(chalk`{green [!] Login Succsess,}{yellow Try to Unfollow All Following ....}`)
+		console.log(chalk`{green [!] Login Succsess Ngopi Yukss,}{yellow Try to Unfollow All Following ....}`)
 		const feed = new Client.Feed.AccountFollowing(doLogin.session, doLogin.account.id);
 		var cursor;
 		do{
 			if (cursor) feed.setCursor(cursor);
 			var getPollowers = await feed.get();
-			getPollowers = _.chunk(getPollowers, 20);
+			getPollowers = _.chunk(getPollowers, 50);
 			for (let i = 0; i < getPollowers.length; i++) {
 				await Promise.all(getPollowers[i].map(async(account) => {
 					const doUnfollow = await Unfollow(doLogin.session, account.id);
 					console.log(chalk`Unfollow {yellow @${account.params.username}} [{cyan ${account.id}}] => ${doUnfollow}`);
 				}));
-				console.log(chalk`{yellow [!] Delay For ${sleep} MiliSeconds}`);
+				console.log(chalk`{yellow [!] Sabar mek ${sleep} MiliSeconds}`);
 				await delay(sleep);
 			}
 			cursor = await feed.getCursor();
 		} while(feed.isMoreAvailable())
-		console.log(chalk`{bold.green [+] Unfollow All Succsess}`)
+		console.log(chalk`{bold.green [+] Memusnakan semua sukses tinggal ngopi doang}`)
 	} catch(e) {
 		console.log(e)
 	}
@@ -85,9 +85,9 @@ const Excute = async function(User,sleep){
 }
 
 console.log(chalk`
-{bold Instagram Unfollow All Following}
-{green BC0DE.NET - NAONLAH.NET - WingKocoli}
-{bold.red Code By Ccocot | ccocot@bc0de.net}
+{bold Instagram Memusnakan Semuanya}
+{green NoMoneyNoLive - CrazyFriends404 - NewYear2k18 | AKAMSI.Pauwa}
+{bold.red Code By pangsitDc0de}
 `);
 
 inquirer.prompt(User)
